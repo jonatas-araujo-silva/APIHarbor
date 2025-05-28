0c66f6710ff3711814b7677b2d4aef78262c95f3
jonatas araujo <jonatasaraujopf@icloud.com>
2025-05-28 08:22:53 -0300

    Initial Commit

---
+++ APIHarbor/APIHarborApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIHarborApp.swift
+//  APIHarbor
+//
+//  Created by Jonatas Araujo on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct APIHarborApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIHarbor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APIHarbor
+//
+//  Created by Jonatas Araujo on 28/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
